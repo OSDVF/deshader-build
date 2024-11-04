35d781f5e7ed630a0b8bf43fbb6e0092c536b0ef
OSDVF <o.s.dv.f@seznam.cz>
2024-11-04 17:57:01 +0100

    Do not always download zig

--- makedeb.bash
+++ makedeb.bash
@@ -15,15 +15,17 @@ s/x86_64/amd64/
 sudo apt update
 sudo apt install -y git
 
-zigver=$(grep -oP "zig=\\K[^']+" PKGBUILD)
-# download zig if the directory does not exist
-zigname=zig-linux-$(arch)-$zigver
-if [ ! -f $zigname ]; then
-    # download zig archive if it does not exist
-    [ -f $zigname.tar.xz ] || wget -O $zigname.tar.xz https://ziglang.org/download/0.13.0/$zigname.tar.xz
-    tar -xf $zigname.tar.xz
+if ! command -v zig; then
+    zigver=$(grep -oP "zig=\\K[^']+" PKGBUILD)
+    # download zig if the directory does not exist
+    zigname=zig-linux-$(arch)-$zigver
+    if [ ! -f $zigname ]; then
+        # download zig archive if it does not exist
+        [ -f $zigname.tar.xz ] || wget -O $zigname.tar.xz https://ziglang.org/download/0.13.0/$zigname.tar.xz
+        tar -xf $zigname.tar.xz
+    fi
+    export PATH=$PWD/$zigname:$PATH
 fi
-export PATH=$PWD/$zigname:$PATH
 yes | tr '[:lower:]' '[:upper:]' | makedeb -s -F PKGBUILD-deb
 
 # rm PKGBUILD-deb
